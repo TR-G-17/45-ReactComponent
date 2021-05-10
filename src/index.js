6f5d68383324bf1a966ecfcdcb11a5f108dda116
Ignaty Kashnitsky <noir.ik@gmail.com>
2021-05-10 09:57:59 +0200

    init project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+      Hello World
+  </div>,
+  document.getElementById('root')
+);
